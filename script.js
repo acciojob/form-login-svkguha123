bce67e0d2f4c8a3f59d7beeed17e316f114594d7
Souvik Guha <svkguha123@gmail.com>
2023-04-27 13:09:13 +0530

    up

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+    let first_name = document.getElementById("firstName").value;
+    let last_name = document.getElementById("lastName").value;
+    alert(first_name+" "+last_name);
 
 }
